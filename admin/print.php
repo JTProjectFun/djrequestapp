b932189440b6468b94e213b78d0539a2d73e1d6c
justin <justin@roger>
2015-04-01 23:45:29 +0100

    better check the event key exists before making the QR code eh

--- admin/print.php
+++ admin/print.php
@@ -1,15 +1,21 @@
 <?php
 include "../configuration.php";
 include "phpqrcode/qrlib.php";
-
+$row = "";
 $key = $_GET['key'];
 $codeContents="http://" . $baseURL . "login.php?key=" . $key;
 
 $tempfile = "images/qrcode.png";
 
-    QRcode::png($codeContents, $tempfile, QR_ECLEVEL_L, 8); 
-         
-    // displaying 
-    echo '<img src="'.$tempfile.'" />'; 
+$conn = mysqli_connect($host,$username,$password,$db);
+$query = mysqli_query($conn, "SELECT thekey FROM requestkeys WHERE thekey='$key'");
+$rows = mysqli_num_rows($query);
+        if ($rows == 1) {
+                             QRcode::png($codeContents, $tempfile, QR_ECLEVEL_L, 8); 
+                             // display QR code
+                             echo '<img src="'.$tempfile.'" />'; 
 
+                         }
+        else echo "<span>INVALID KEY SUPPLIED</span>";
+mysqli_close($conn);
 ?>
