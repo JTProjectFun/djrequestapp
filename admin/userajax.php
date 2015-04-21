5d52355caa717cda0a6d138b1e28ea19b062a82e
Justin Hornsby <justin@justinhornsby.co.uk>
2015-04-21 19:59:45 +0100

    Think this finally fixes the user listing issues. Maybe.

--- admin/userajax.php
+++ admin/userajax.php
@@ -13,16 +13,32 @@ if (isset($_SESSION['listuser'])) {
 switch($action) {
 	case "load":
                 $rq = mysqli_connect($host, $username, $password, $db);
-                if ($luser == 0){
-                    $querystring = "SELECT requestusers.* FROM requestusers LEFT JOIN requestkeys ON requestusers.thekey=requestkeys.thekey";
+                switch($userlevel) {
+                    case 1: // Only show requests from this admin user's events on key = 0
+                        $query_string = "SELECT requestusers.* FROM requestusers LEFT JOIN requestkeys ON requestusers.thekey=requestkeys.thekey WHERE requestkeys.userid='".$userid."'";
+                        if ($luser != "0") {
+                            $query_string = $query_string . " AND requestusers.uniqueid='".$luser;
+                        }
+                        break;
+
+                    case 2: // Only show requests from this admin user's events on key = 0
+                        $query_string = "SELECT requestusers.* FROM requestusers LEFT JOIN requestkeys ON requestusers.thekey=requestkeys.thekey WHERE requestkeys.userid='".$userid."'";
+                        if ($luser != "0") {
+                            $query_string = $query_string . " AND requestusers.uniqueid='".$luser."'";
+                        }
+                        break;
+
+                    case 3: // Only show requests from this admin user's events on key = 0
+                        if ($luser == "0") {
+                            $query_string = "SELECT * from requestusers";
+                        }
+                        else {
+                            $query_string = "SELECT * FROM requestusers WHERE uniqueid='".$luser."'";
+                        }
+                        break;
                 }
-                else {
-                    $querystring = "SELECT * FROM requestusers WHERE uniqueid='".$luser."'";
-                }
-                if ($userlevel != "3") {
-                    $querystring = $querystring . " WHERE requestkeys.userid='".$userid."'";
-                }
-                $query = mysqli_query($rq, $querystring);
+
+                $query = mysqli_query($rq, $query_string);
 		$count  = mysqli_num_rows($query);
 		if($count > 0) {
 			while($fetch = mysqli_fetch_array($query)) {
@@ -47,7 +63,7 @@ switch($action) {
             if($count <= 0) {
             ?>
             <tr id="norecords">
-                <td colspan="11" align="center">No records found<</td>
+                <td colspan="11" align="center">No records found</td>
             </tr>
             <?php } else {
             $i = 0;
