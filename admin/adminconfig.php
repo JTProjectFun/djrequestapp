803682de28fe2d8de0a35af249326c9854d4bd0d
Justin Hornsby <justin0hornsby@gmail.com>
2016-03-22 12:11:52 +0000

    Don't trust cookies y'all

--- admin/adminconfig.php
+++ admin/adminconfig.php
@@ -1,7 +1,8 @@
 <?php
 
-$user = $_COOKIE['adminuser'];
-$userid = $_COOKIE['adminuserid'];
-$userlevel = $_COOKIE['adminlevel'];
-$adminrealname = $_COOKIE['adminrealname'];
+// Reminder: DON'T TRUST COOKIES!
+$user = preg_replace('/[^\w]/', '', $_COOKIE['adminuser']);
+$userid = preg_replace('/[^\w]/', '', $_COOKIE['adminuserid']);
+$userlevel = preg_replace('/[^\w]/', '',  $_COOKIE['adminlevel']);
+$adminrealname = preg_replace('/[^\w]/', '', $_COOKIE['adminrealname']);
 ?>
